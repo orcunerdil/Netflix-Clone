25cd576b5dc6229609e8559052b3296600e3d5a4
Orcun Erdil <orcun_erdil@hotmail.com>
2023-06-30 22:36:38 +0300

    Initial Commit

---
+++ Netflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by Orçun Erdil on 30.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
