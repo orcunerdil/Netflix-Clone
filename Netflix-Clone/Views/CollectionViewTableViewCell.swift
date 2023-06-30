c061c6c257e78107c9b5cf10842b3eca052bccaf
Orcun Erdil <orcun_erdil@hotmail.com>
2023-07-01 00:57:47 +0300

    created header view

---
+++ Netflix-Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix-Clone
+//
+//  Created by Orçun Erdil on 30.06.2023.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Netflix-Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix-Clone
+//
+//  Created by Orçun Erdil on 1.07.2023.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
